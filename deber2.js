0a1a997bbb6e4af79062194f2f537657544d4d07
boris2442 <aubinborissimotsebo@gmail.com>
2024-12-26 12:47:17 +0100

    introduction de la librairie scroll reveal

--- deber2.js
+++ deber2.js
@@ -20,27 +20,57 @@ const app2=document.getElementById("titre2");
 
 const typewriter = new Typewriter(app, {
   loop: true,
-  delay: 75,
+  delay: 50,
 });
 
-typewriter
-  .typeString("Roulez avec de l'expérience")
-  .pauseFor(2000)
-  .deleteAll()
-  .start();
+// typewriter
+//   .typeString("Roulez avec de l'expérience")
+//   .pauseFor(2000)
+//   .deleteAll()
+//   .start();
 
 
   const typewriter2 = new Typewriter(app2,{
   loop:true,
-  delay:100,
+  delay:75,
 
 
   })
 
-  typewriter2
-  .typeString("Location de voiture simplifiée !")
+  // typewriter2
+  // .typeString("Location de voiture simplifiée !")
+  // .pauseFor(2000)
+  // .deleteAll()
+  // .start();
+
+  // startTypewriterLoop();
+function startTypewriterLoop(){
+
+  typewriter
+  .typeString("Roulez avec de l'expérience")
   .pauseFor(2000)
   .deleteAll()
-  .start();
+  .callFunction(()=>{
 
-  startTypewriterLoop();
+    typewriter2
+    .typeString("Location de voiture simplifiée !")
+    .pauseFor(2000)
+    .deleteAll()
+    .callFunction(startTypewriterLoop)
+    .start();
+  })
+  .start();
+}
+startTypewriterLoop();
+
+
+
+// partie destinee au scroll reveal
+
+const sr=ScrollReveal({
+  duration:1500,
+  origin:"top",
+  distance:"30px",
+  reset:true
+})
+sr.reveal(".copy, .first-section, .cars, .third-section, .section-fourth, .sixth-section, .fifth-section, .child-footer ", {interval:200})
